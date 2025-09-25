cfce4e1d4c70833f26657cc8a13901c3b2ce4c49
SuhAilManzoor <sohailmanzoorsario@gmail.com>
2025-09-25 22:58:17 +0500

    Profile View have a counter and Add button when you tapping the button calls a method defined in Profile View Model to increment the counter value and rebuild the UI with updated counter value

---
+++ lib/screens/profile/profile_view.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+import 'package:sixteen_stacked_state_mangement_withoutservice_pt1/screens/profile/profile_viewmodel.dart';
+
+class ProfileView extends StatelessWidget {
+  const ProfileView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder<ProfileViewModel>.reactive(
+      viewModelBuilder: () => ProfileViewModel(),
+      builder: (context, viewModel, child) {
+        return Scaffold(
+          appBar: AppBar(title: Text("Profile")),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  viewModel.counter.toString(),
+                  style: TextStyle(fontSize: 40),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    viewModel.CountedValue();
+                  },
+                  child: Text("Count"),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
